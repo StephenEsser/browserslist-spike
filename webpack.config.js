41fffdca0ab7ff07e8bbaa73dee548e7d4e10d17
Stephen Esser <stephen.esser@cerner.com>
2020-08-25 12:29:11 -0500

    Remove unneeded webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,7 @@ const appWebpackConfig = () => ({
 });
 
 const mergedConfig = (env, argv) => {
-  const config = merge(toolkitWebpackConfig(env, argv), appWebpackConfig(env, argv));
-
-  // This is required to bundle the web compatible octokit/rest.
-  config.resolve.mainFields = ['browser', 'module', 'main'];
-
-  return config;
+  return merge(toolkitWebpackConfig(env, argv), appWebpackConfig(env, argv));
 };
 
 module.exports = mergedConfig;
